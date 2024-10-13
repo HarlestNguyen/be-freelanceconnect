b1314c15e22538db3a3042689e8c2997f60df9f6
Khang0202 <nguyenmanhkhang2002@gmail.com>
2024-10-13 14:41:13 +0700

    update data

--- src/main/java/vn/com/easyjob/base/BaseServiceDTO.java
+++ src/main/java/vn/com/easyjob/base/BaseServiceDTO.java
@@ -6,57 +6,55 @@ import vn.com.easyjob.exception.ErrorHandler;
 import java.lang.reflect.Field;
 import java.util.Collection;
 
-public abstract class BaseServiceDTO<T extends BaseEntity, DTO, ID> extends BaseService {
+public abstract class BaseServiceDTO<T extends BaseEntity, DTO, ID> extends BaseService<T, ID> implements IService<T, ID>  {
 
     // Phương thức để map đối tượng kiểu T (entity) sang DTO
-    private DTO toDTO(BaseEntity t, Class<DTO> dtoClass) {
-        try {
-            // Tạo một thể hiện mới của DTO
-            DTO dto = dtoClass.getDeclaredConstructor().newInstance();
+//    private DTO toDTO(BaseEntity t, Class<DTO> dtoClass) {
+//        try {
+//            // Tạo một thể hiện mới của DTO
+//            DTO dto = dtoClass.getDeclaredConstructor().newInstance();
+//
+//            // Lấy tất cả các field của entity
+//            Field[] entityFields = t.getClass().getDeclaredFields();
+//
+//            // Lặp qua các field trong entity
+//            for (Field entityField : entityFields) {
+//                entityField.setAccessible(true); // Truy cập các private field
+//                Object value = entityField.get(t); // Lấy giá trị của field từ entity
+//
+//                try {
+//                    // Tìm field tương ứng trong DTO theo tên
+//                    Field dtoField = dtoClass.getDeclaredField(entityField.getName());
+//                    dtoField.setAccessible(true);
+//
+//                    // Kiểm tra nếu kiểu dữ liệu của field trong DTO và entity tương thích
+//                    if (dtoField.getType().isAssignableFrom(entityField.getType())) {
+//                        dtoField.set(dto, value); // Gán giá trị từ entity sang DTO
+//                    }
+//
+//                } catch (NoSuchFieldException e) {
+//                    // Nếu DTO không có field tương ứng, bỏ qua field này
+//                    continue;
+//                }
+//            }
+//
+//            return dto; // Trả về đối tượng DTO đã được map
+//        } catch (Exception e) {
+//            throw new ErrorHandler(HttpStatus.INTERNAL_SERVER_ERROR, "Error while mapping entity to DTO: " + e.getMessage());
+//        }
+//    }
 
-            // Lấy tất cả các field của entity
-            Field[] entityFields = t.getClass().getDeclaredFields();
+    protected abstract BaseMapper<T, DTO> getMapper();
 
-            // Lặp qua các field trong entity
-            for (Field entityField : entityFields) {
-                entityField.setAccessible(true); // Truy cập các private field
-                Object value = entityField.get(t); // Lấy giá trị của field từ entity
-
-                try {
-                    // Tìm field tương ứng trong DTO theo tên
-                    Field dtoField = dtoClass.getDeclaredField(entityField.getName());
-                    dtoField.setAccessible(true);
-
-                    // Kiểm tra nếu kiểu dữ liệu của field trong DTO và entity tương thích
-                    if (dtoField.getType().isAssignableFrom(entityField.getType())) {
-                        dtoField.set(dto, value); // Gán giá trị từ entity sang DTO
-                    }
-
-                } catch (NoSuchFieldException e) {
-                    // Nếu DTO không có field tương ứng, bỏ qua field này
-                    continue;
-                }
-            }
-
-            return dto; // Trả về đối tượng DTO đã được map
-        } catch (Exception e) {
-            throw new ErrorHandler(HttpStatus.INTERNAL_SERVER_ERROR, "Error while mapping entity to DTO: " + e.getMessage());
-        }
+    public DTO saveToDTO(T t) {
+        return getMapper().toDto(super.save(t));
     }
 
-    public DTO save(T t, Class<DTO> dtoClass) {
-        return toDTO(save(t), dtoClass);
+    public Collection<DTO> findAllToDTO() {
+        return super.findAll().stream().map(t -> getMapper().toDto(t)).toList();
     }
 
-    public Collection<DTO> findAll(Class<DTO> dtoClass) {
-        try {
-            return findAll().stream().map(t -> toDTO((BaseEntity) t, dtoClass)).toList();
-        } catch (Exception e) {
-            throw new ErrorHandler(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-        }
-    }
-
-    public DTO findOne(ID id, Class<DTO> dtoClass) {
-        return toDTO(findOne(id), dtoClass);
+    public DTO findOneToDTO(ID id) {
+        return getMapper().toDto(super.findOne(id));
     }
 }
--- src/main/java/vn/com/easyjob/service/job/JobSkillServiceImpl.java
+++ src/main/java/vn/com/easyjob/service/job/JobSkillServiceImpl.java
@@ -2,18 +2,30 @@ package vn.com.easyjob.service.job;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vn.com.easyjob.base.BaseMapper;
 import vn.com.easyjob.base.BaseService;
+import vn.com.easyjob.base.BaseServiceDTO;
 import vn.com.easyjob.base.IRepository;
+import vn.com.easyjob.model.dto.JobSkillDTO;
 import vn.com.easyjob.model.entity.JobSkill;
+import vn.com.easyjob.model.mapper.JobSkillMapper;
 import vn.com.easyjob.repository.JobSkillRepository;
 
 @Service
-public class JobSkillServiceImpl extends BaseService<JobSkill, Long> implements JobSkillService {
+public class JobSkillServiceImpl extends BaseServiceDTO<JobSkill, JobSkillDTO, Long> implements JobSkillService {
     @Autowired
     private JobSkillRepository jobSkillRepository;
+
+    @Autowired
+    private JobSkillMapper jobSkillMapper;
 
     @Override
     protected IRepository<JobSkill, Long> getRepository() {
         return jobSkillRepository;
+    }
+
+    @Override
+    protected BaseMapper<JobSkill, JobSkillDTO> getMapper() {
+        return jobSkillMapper;
     }
 }
